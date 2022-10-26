6c8cf32be198b52351b8cc9218171f5ee44246e5
MahmoudReda11 <115508120+MahmoudReda11@users.noreply.github.com>
2022-10-27 00:11:44 +0200

    Add files via upload

---
+++ js/home.js
@@ -0,0 +1,60 @@
+let menu = document.querySelector('#menu');
+let header = document.querySelector('header');
+let navbar = document.querySelector('.navbar');
+let liMenu = document.querySelectorAll('.navbar ul li');
+let sign = document.getElementById('sign');
+let allSign = document.getElementById('sgn-reg-all');
+let closee = document.getElementById('close');
+
+
+menu.onclick = function (){
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+  liMenu.forEach(li => {
+    li.onclick= function (){
+      menu.classList.remove('fa-times');
+      navbar.classList.remove('active');
+    }
+  });
+}
+
+window.onscroll = function (){
+  if(this.scrollY >= 120){
+    header.style.background = '#4444449c'
+  }else{
+    header.style.background = 'transparent'
+  }
+}
+
+
+// swiber team
+
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
+  pagination:{
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints:{
+    0: {
+      slidesPerView: 1.3,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 3,
+    },
+  },
+});
